84e14a9029a12b7dc430db3167ec2cda7b2de79f
Anthonnytauchen <162629790+Anthonnytauchen@users.noreply.github.com>
2026-02-20 16:06:53 -0300

    first commit

---
+++ src/main/java/project/booteco/BootEcoApplication.java
@@ -0,0 +1,13 @@
+package project.booteco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootEcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootEcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/booteco/BootEcoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.booteco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootEcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
