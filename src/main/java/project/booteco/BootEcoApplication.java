907e6983be6bf4235a42d97c669a296db042216b
Anthonnytauchen <162629790+Anthonnytauchen@users.noreply.github.com>
2026-02-21 15:38:04 -0300

    segundo commit

--- src/main/java/project/booteco/BootEcoApplication.java
+++ src/main/java/project/booteco/BootEcoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootEcoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootEcoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootEcoApplication.class, args);
+    }
 
 }
--- src/test/java/project/booteco/BootEcoApplicationTests.java
+++ src/test/java/project/booteco/BootEcoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BootEcoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
